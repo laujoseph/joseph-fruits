8447d4885347dae0bb4676833eddc33454cc34e0
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-06-10 14:29:13 +0800

    First commit

---
+++ mongoose_fruits/server.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
